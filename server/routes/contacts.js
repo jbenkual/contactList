47a5300741c41168a4d85e7bc820ddadec147b29
jbenkual <joe@benkual.com>
2015-10-08 10:59:30 -0700

    Add code to create and retrieve contacts

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../db");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(200);
+  db.getContacts(function(contacts) {
+    res.send(contacts);
+  })
+  
+});
+
+router.post('/', function(req, res, next) {
+  var contact = db.createContact(req.body);
+  db.save(contact, function(data) {
+    res.send(data);
+  })
 });
 
 module.exports = router;
